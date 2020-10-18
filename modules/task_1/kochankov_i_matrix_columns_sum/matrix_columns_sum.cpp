256c00fcd1c48ae131cfbdcfa8a11767086a233e
Ilya Kochankov <ilyakochankov@MBP-Ilya.Dlink>
2020-10-18 14:41:08 +0300

    project init

---
+++ modules/task_1/kochankov_i_matrix_columns_sum/matrix_columns_sum.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ilya Kochankov on 10/18/20.
+//
+
+#include "matrix_columns_sum.h"
---
+++ modules/task_1/kochankov_i_matrix_columns_sum/matrix_columns_sum.h
@@ -0,0 +1,14 @@
+//
+// Created by Ilya Kochankov on 10/18/20.
+//
+
+#ifndef PP_2020_AUTUMN_ENGINEER_MATRIX_COLUMNS_SUM_H
+#define PP_2020_AUTUMN_ENGINEER_MATRIX_COLUMNS_SUM_H
+
+
+class matrix_columns_sum {
+
+};
+
+
+#endif //PP_2020_AUTUMN_ENGINEER_MATRIX_COLUMNS_SUM_H
